cb9eaffaf97a14ef142e6c144dc9a4351b4437f6
johnathan crocker <johnathan.crocker@gmail.com>
2017-09-23 15:56:59 -0500

    Removed Morgan

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var app = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-var morgan = require('morgan');
 var cookieparser = require('cookie-parser');
 var bodyparser = require('body-parser');
 var session = require('express-session');
@@ -19,7 +18,6 @@ var configDB = require('./config/database.js');
 mongoose.connection.openUri(configDB.uri);
 
 //setup express
-app.use(morgan('dev'));
 app.use(cookieparser());
 app.use(bodyparser());
 app.use(express.static("./public"));
