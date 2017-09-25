3df7cf2a50b4d480f1806a4ebd3c3df67bae5681
johnathan crocker <johnathan.crocker@gmail.com>
2017-09-25 14:29:54 -0500

    Designed the User model

---
+++ app/models/User.js
@@ -0,0 +1,22 @@
+// /app/models/User.js
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+    userName : String,
+    password : String,
+    admin : Boolean,
+    moderator : Boolean,
+    banned : Boolean
+});
+
+//Methods
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+}
