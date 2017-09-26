9321c043229c055c02d14b6aded68d1c4c54cac4
johnathan crocker <johnathan.crocker@gmail.com>
2017-09-25 19:01:22 -0500

    Working on registration

--- routes/route_account.js
+++ routes/route_account.js
@@ -1,11 +1,23 @@
 // /routes/route_account.js
 
 module.exports = function(config, app, passport) {
-    
+    //Account registration form
     app.get('/account/register', function(req, res) {
+        if(req.query.error) {
+            req.flash("registerError", "An account with that email already exists.");
+        } else if(req.query.success) {
+            req.flash("registerSuccess", "You have successfuly registered. You may now log in.");
+        }
         res.render('./account/register.ejs', {
             title : config.title,
+            registerError : req.flash("registerError"),
+            registerSuccess : req.flash("registerSuccess"),
             loggedIn: req.isAuthenticated()
         })
+    });
+
+    //Process account registration form
+    app.post('/account/register', function(req, res) {
+
     })
 }
